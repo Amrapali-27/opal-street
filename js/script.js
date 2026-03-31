21560bb3ae942ca8a68639beee00e1c695040dd6
Amrapali-27 <amrapaligoswami33@gmail.com>
2026-03-31 16:56:21 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("navMenu");
+
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+
+});
